4551caa1b17e67982e654775880144ceea92ba62
Niharika <niharika.3297@gmail.com>
2020-04-11 16:56:04 +0530

    make it better

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+#![feature(proc_macro_hygiene, decl_macro)]
 
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
 
-async fn index2() -> impl Responder {
-    HttpResponse::Ok().body("Hello world again!")
-}
-
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(index))
-            .route("/again", web::get().to(index2))
-    })
-    .bind("127.0.0.1:8088")?
-    .run()
-    .await
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
 }
