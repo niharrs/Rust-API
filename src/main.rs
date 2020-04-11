c080db9c74d5312fbedcbdf0c15f2a0d1c2761dc
Niharika <niharika.3297@gmail.com>
2020-04-11 18:07:03 +0530

    make it better

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::env;
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -10,12 +11,15 @@ async fn index2() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    let host = env::var("HOST").expect("Host not set");
+    let port = env::var("PORT").expect("Port not set");
+
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
             .route("/again", web::get().to(index2))
     })
-    .bind("0.0.0.0:8088")?
+    .bind(format!("{}:{}", host, port))?
     .run()
     .await
 }
